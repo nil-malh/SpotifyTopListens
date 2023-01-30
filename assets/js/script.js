8bd7c844771e200e6b6fdebcef2b412d9f5313fe
Nil MALHOMME <malhomme.nil+github@gmail.com>
2023-01-30 22:33:12 +0100

    Added the possibility to choose the time period, removed the loader seems to be not friendly with GitHub Pages, removed useless functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,9 @@
 const clientID = "redacted";
 const clientSecret = "redacted";
 const authBasic = btoa(clientID + ':' + clientSecret).toString('base64');
-
-function retriveOAuthToken()
-{
-    let authToken ="";
-    let body = new URLSearchParams();
-    body.append("grant_type", "client_credentials");
-      let bodyString = JSON.stringify(body);
-  new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            console.log(xhr.status);
-            if (xhr.status === 200) {
-                if (!xhr.response) {
-                    reject([500, 'Something unknown went wrong']);
-                } else {
-                    resolve(JSON.parse(xhr.response));
-                }
-            } else {
-                reject([xhr.status, xhr.response]);
-            }
-        }
-    };
-    xhr.open('POST', 'https://accounts.spotify.com/api/token', true);
-    xhr.setRequestHeader('Authorization', `Basic ${authBasic}` );
-    xhr.setRequestHeader( 'Content-Type',   'application/x-www-form-urlencoded' );
-
-    xhr.send(body);
-}).then((value) => {
-    console.log(`Success ! Retrived authToken : ${value.access_token}`);
-    authToken = value.access_token;
-}).catch((value) => {
-    console.log(value);
-});
-    return authToken;
-}
-
-//const authToken = retriveOAuthToken();
-
-function aboutSong(authToken , trackID="0")
-{
-
-new Promise((resolve, reject) => {
-
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            console.log(xhr.status);
-            if (xhr.status === 200) {
-                if (!xhr.response) {
-                    reject([500, 'Something unknown went wrong']);
-                } else {
-                    resolve(JSON.parse(xhr.response));
-                }
-            } else {
-                reject([xhr.status, xhr.response]);
-            }
-        }
-    };
-    let token = retriveOAuthToken();
-    xhr.open('GET', 'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V', true);
-    xhr.setRequestHeader('Authorization', `Bearer BQCCkHkA1HeByHYIuQoMVNMP0DOSu8_y5arCsANv4uq7uX6Bfa_-7q2V_wBRnBKJm1Oauh33wFwTCzeMWb72wMzv45ClBLmq70g_-B3dFQdNEsqrhJSS` );
-    xhr.setRequestHeader( 'Content-Type',   'application/x-www-form-urlencoded' );
-
-    xhr.send();
-}).then((value) => {
-    console.log(`Success ! Retrived data : ${value.album.name}`);
-}).catch((value) => {
-    console.log(value);
-});
-
-}
-
-/*
-curl -X "GET" "https://api.spotify.com/v1/me/top/tracks" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQCdv3XMlu0ndbMhFpM2HQ2ZUhsIsolIa6kw3h9pPkduqBvbXcIshSgz_b9RMeX9MFdqnbr6ZXAeD-O88ddRgxKUU7d3UekYlv2FSWPsNc8iBYOKYGhEDZBvRz05OeGyiiKWtE5RjbZO1WHgg-UzC7RVuoKH4ry9oMRtNjzTBJuTYEYX00DHC78tTcE"
-*/
-
 let errorMessage;
-function getUserTopSongs(authToken)
+
+function getUserTopSongs(authToken,timePeriod="short_term")
 {
     let albums = [];
     new Promise((resolve, reject) => {
@@ -99,7 +23,7 @@ function getUserTopSongs(authToken)
                 }
             }
         };
-        xhr.open('GET', 'https://api.spotify.com/v1/me/top/tracks', true);
+        xhr.open('GET', `https://api.spotify.com/v1/me/top/tracks?time_range=${timePeriod}`, true);
         xhr.setRequestHeader('Authorization', `Bearer ${authToken}` );
         xhr.setRequestHeader( 'Content-Type',   'application/x-www-form-urlencoded' );
     
@@ -116,7 +40,7 @@ function getUserTopSongs(authToken)
             const artistName = album.album.artists[0].name;
             const songName = album.name;
             const albumCoverURL = album.album.images[0].url;
-            console.log(album.album.images[0].url);
+            //console.log(album.album.images[0].url);
             rank++;
             let img = document.createElement("img");
             img.className = "artwork mt-3";
@@ -132,9 +56,7 @@ function getUserTopSongs(authToken)
             songContainer.appendChild(p);
             divSongContainer.appendChild(songContainer);
         }
-        document.body.appendChild(divSongContainer)
-        console.log(`Success ! Retrived data : ${response.items}`);
-        
+        document.body.appendChild(divSongContainer)        
       
     }).catch((value) => {
         errorMessage = document.createElement("p");
@@ -162,6 +84,8 @@ let userToken = undefined;
         redirectButton.href = "https://developer.spotify.com/console/get-current-user-top-artists-and-tracks/?type=tracks&time_range=&limit=&offset="
         redirectButton.innerText = "Click here !"
 
+        let form = document.createElement("form");
+
         let formDiv = document.createElement("div");
             formDiv.className = "inputbox"
 
@@ -172,9 +96,58 @@ let userToken = undefined;
         inputToken.className = "input mt-1";
         inputToken.required = "required";
 //*<label for="name" class="form__label">Name</label>
-        let labelToken = document.createElement("span");
+   
 
-        let isep = document.createElement("i");
+/*
+<form>
+  <p>Veuillez choisir la meilleure méthode pour vous contacter :</p>
+  <div>
+    <input type="radio" id="contactChoice1"
+     name="contact" value="email">
+    <label for="contactChoice1">Email</label>
+
+    <input type="radio" id="contactChoice2"
+     name="contact" value="telephone">
+    <label for="contactChoice2">Téléphone</label>
+
+    <input type="radio" id="contactChoice3"
+     name="contact" value="courrier">
+    <label for="contactChoice3">Courrier</label>
+  </div>
+*/
+// Create radio input for "2 weeks" option
+var radio2Weeks = document.createElement("input");
+radio2Weeks.type = "radio";
+radio2Weeks.name = "timeRange";
+radio2Weeks.value = "short_term";
+
+// Create label for "2 weeks" option
+var label2Weeks = document.createElement("label");
+label2Weeks.innerHTML = "4 weeks";
+
+// Create radio input for "6 months" option
+var radio6Months = document.createElement("input");
+radio6Months.type = "radio";
+radio6Months.name = "timeRange";
+radio6Months.value = "medium_term";
+
+// Create label for "6 months" option
+var label6Months = document.createElement("label");
+label6Months.innerHTML = "6 months";
+
+// Create radio input for "all time" option
+var radioAllTime = document.createElement("input");
+radioAllTime.type = "radio";
+radioAllTime.name = "timeRange";
+radioAllTime.value = "long_term";
+
+// Create label for "all time" option
+var labelAllTime = document.createElement("label");
+labelAllTime.innerHTML = "all time";
+
+// Add radio inputs and labels to form
+
+
 
     let buttonToken = document.createElement("button");
     buttonToken.className = "button-submit mt-1";
@@ -183,10 +156,17 @@ let userToken = undefined;
     // AddToken an event listener to the button
     buttonToken.addEventListener("click", function(){
         // Get the value of the input text field using querySelector
-        var inputValue = document.querySelector("#token").value;
+        let inputValue = document.querySelector("#token").value;
         // Do something with the input value, e.g. alert it
-        getUserTopSongs(inputValue)
-        errorMessage.remove();
+        let timeRange = document.querySelector('input[name="timeRange"]:checked');
+        if(timeRange === null)
+            timeRange = "short_term";   
+        timeRange = timeRange.value;
+        console.log("Selected time range: " + timeRange);
+
+        getUserTopSongs(inputValue,timeRange)
+        if(errorMessage !== null)
+                errorMessage.remove();
     });
 
 
@@ -194,10 +174,15 @@ let userToken = undefined;
         loginDiv.appendChild(redirectButton);
 
         formDiv.appendChild(inputToken);
-        formDiv.appendChild(labelToken);
-        formDiv.appendChild(isep);
+        formDiv.appendChild(radio2Weeks);
+        formDiv.appendChild(label2Weeks);
+        formDiv.appendChild(radio6Months);
+        formDiv.appendChild(label6Months);
+        formDiv.appendChild(radioAllTime);
+        formDiv.appendChild(labelAllTime);
 
-        loginDiv.appendChild(formDiv); 
+        form.appendChild(formDiv);
+        loginDiv.appendChild(form); 
         loginDiv.appendChild(buttonToken);
 
         document.body.appendChild(loginDiv);    
